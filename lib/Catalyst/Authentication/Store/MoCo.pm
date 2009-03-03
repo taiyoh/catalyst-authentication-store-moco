64b15e91e42e72d2ddb940e36a8796da545fa151
Taiyoh Tanaka <sun.basix@gmail.com>
2009-03-03 17:31:06 +0900

    initial commit

---
+++ lib/Catalyst/Authentication/Store/MoCo.pm
@@ -0,0 +1,33 @@
+package Catalyst::Authentication::Store::MoCo;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Catalyst::Authentication::Store::MoCo -
+
+=head1 SYNOPSIS
+
+  use Catalyst::Authentication::Store::MoCo;
+
+=head1 DESCRIPTION
+
+Catalyst::Authentication::Store::MoCo is
+
+=head1 AUTHOR
+
+taiyoh E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
